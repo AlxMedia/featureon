89e6987b0ac8f73ce96de1e360417e54f7a4fa39
AlxMedia <contact@alx.media>
2022-02-23 12:05:35 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -684,7 +684,7 @@ Kirki::add_field( 'featureon_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'featureon' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'featureon' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'featureon' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'featureon' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'featureon' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'featureon' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'featureon' ),
@@ -701,7 +701,7 @@ Kirki::add_field( 'featureon_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'featureon' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'featureon' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'featureon' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'featureon' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'featureon' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
