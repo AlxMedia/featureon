9e5e9d3afa8b5f0bbd174f7041094ce7f07fb9a1
Alexander Agnarson <contact@alx.media>
2025-04-09 20:22:11 +0200

    Kirki deprecated fix

--- functions.php
+++ functions.php
@@ -757,3 +757,16 @@ function featureon_skip_link_focus_fix() {
 	<?php
 }
 add_action( 'wp_print_footer_scripts', 'featureon_skip_link_focus_fix' );
+
+
+/*  Kirki deprecated fix
+/* ------------------------------------ */
+function featureon_kirki_config( $config ) {
+
+	if ( isset( $config['compiler'] ) ) {
+		unset( $config['compiler'] );
+	}
+
+	return $config;
+}
+add_filter( 'kirki/config', 'featureon_kirki_config', 999 );
